4fd401cfa86c364c8ddab0d156f26e7516a3a056
Igor Hollaender <igor.hollaender@gmail.com>
2023-12-11 15:23:38 +0100

    basic HTTP server works in BA

--- code.py
+++ code.py
@@ -26,9 +26,9 @@ print(f"SSID:     {os.getenv('CIRCUITPY_WIFI_SSID')}")
 print(f"Password: {os.getenv('CIRCUITPY_WIFI_PASSWORD')}")
 
 #  set static IP address
-ipv4    =  ipaddress.IPv4Address("192.168.0.110")
-netmask =  ipaddress.IPv4Address("255.255.255.0")
-gateway =  ipaddress.IPv4Address("192.168.1.1")
+ipv4    =  ipaddress.IPv4Address("192.168.0.110")   #IH231211 "192.168.0.110" works in BA
+netmask =  ipaddress.IPv4Address("255.255.255.0")   #IH231211 not sure
+gateway =  ipaddress.IPv4Address("192.168.1.1")     #IH231211 not sure
 wifi.radio.set_ipv4_address(ipv4=ipv4,netmask=netmask,gateway=gateway)
 
 #  connect to your SSID
