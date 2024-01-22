1b816de8692df18216da0eb5bb2a7ab98c94dcd4
Igor Hollaender (BV) <igor.hollaender@gmail.com>
2024-01-22 15:05:23 +0100

    wip 240122b

---
+++ FountainSimulatedRTC.py
@@ -0,0 +1,21 @@
+#
+#    F o u n t a i n   S i m u l a t e d   R T C . p y 
+#
+#    Last revision: IH240122
+#
+#    RTC simulated by HTTP request to a standard site
+
+class FountainSimulatedRTC():
+
+    def __init__(
+                self, 
+                debug=True) -> None:
+
+        self.debug = debug
+        
+
+    
+
+
+
+
