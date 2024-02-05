c734c0fd1a359cd89c9c7076ff41e383f0384b8b
Igor Hollaender (BV) <igor.hollaender@gmail.com>
2024-02-05 14:07:15 +0100

    wip 240205b

---
+++ FountainDeviceStatusVisualizer.py
@@ -0,0 +1,17 @@
+#
+#    F o u n t a i n   D e v i c e   S t a t u s   V i s u a l i z e r . p y 
+#
+#    Last revision: IH240205
+
+
+class FountainDeviceStatusVisualizer():
+
+    def __init__(
+                self, 
+                showSchedule,
+                startDelayMilliSeconds=0,
+                debug=True) -> None:
+
+        pass
+
+    def setSchedule(self,schedule):
